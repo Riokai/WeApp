1948d4b04f94cd2cd0c02b82c221665c2466e5e3
Kai <zengkai2009@foxmail.com>
2016-10-25 11:57:27 +0800

    add section data

--- src/page/Main.js
+++ src/page/Main.js
@@ -8,6 +8,10 @@ import {
   ScrollView
 } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import IonIcon from 'react-native-vector-icons/Ionicons'
+import moduleConfig from '../config/module.json'
+
+
 
 export default class Main extends Component {
   handlePress() {
@@ -24,35 +28,24 @@ export default class Main extends Component {
   render() {
     return (
       <ScrollView style={styles.scrollView}>
-        <TouchableHighlight>
-          <View style={[styles.section, styles.bgRed]}>
-            <View style={styles.iconWrapper}>
-              <Icon name="rocket" size={30} color="#900" />
-            </View>
-            <View style={styles.textWrapper}>
-              <Text>Section 1</Text>
-            </View>
-          </View>
-        </TouchableHighlight>
+        {
+          moduleConfig.map(item => {
+            return (
+              <TouchableHighlight onPress={this.handlePress} key={item.name}>
+                <View style={[styles.section, { backgroundColor: item.bgcolor }]}>
+                  <View style={styles.iconWrapper}>
+                    {/* <Icon name={item.icon} size={40} color="#fff" /> */}
+                    <IonIcon name={item.icon} size={40} color="#fff" />
+                  </View>
+                  <View style={styles.textWrapper}>
+                    <Text style={styles.text}>{item.name}</Text>
+                  </View>
+                </View>
+              </TouchableHighlight>
+            )
+          })
+        }
       </ScrollView>
-
-
-      // <View style={styles.container}>
-      //   <TouchableHighlight style={styles.touchable} underlayColor='transparent' onPress={this.handlePress}>
-      //     <View style={[styles.section, styles.bgRed]}>
-      //     </View>
-      //   </TouchableHighlight>
-      //   <TouchableHighlight style={styles.touchable} underlayColor='transparent' onPress={this.handlePress}>
-      //     <View style={[styles.section, styles.bgGreen]}>
-      //         <Text style={styles.text}>Section 2</Text>
-      //     </View>
-      //   </TouchableHighlight>
-      //   <TouchableHighlight style={styles.touchable} underlayColor='transparent' onPress={this.handlePress}>
-      //     <View style={[styles.section, styles.bgBlue]}>
-      //         <Text style={styles.text}>Section 3</Text>
-      //     </View>
-      //   </TouchableHighlight>
-      // </View>
     )
   }
 }
@@ -62,24 +55,30 @@ const styles = StyleSheet.create({
     paddingTop: 30
   },
   iconWrapper: {
-    width: 40
+    width: 80,
+    paddingHorizontal:20
   },
   container: {
     paddingTop: 30,
-    paddingHorizontal: 10
+    paddingHorizontal: 8
   },
   section: {
-    height: 40,
+    height: 80,
     // display: 'flex',
     flex: 1,
+    flexDirection: 'row',
     // justifyContent: 'center',
-    // alignItems: 'center',
+    alignItems: 'center',
     // borderRadius: 10,
     // overflow: 'hidden'
   },
   text: {
-    textAlign: 'center',
-    color: 'white'
+    position: 'relative',
+    top: -2,
+    left: -5,
+    textAlign: 'left',
+    color: 'white',
+    fontSize: 20
   },
   bgRed: {
     backgroundColor: 'red',
